daf59d8665b81489ca19ecc0a5c7a3c0bb596e96
Mohamed <muhammadelashri@gmail.com>
2023-03-31 17:09:22 -0400

    Remove editbutton DOM

--- app.js
+++ app.js
@@ -1,45 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const latexInput = document.getElementById('latexInput');
-    const convertBtn = document.getElementById('convertBtn');
-    const mathmlOutput = document.getElementById('mathmlOutput');
-    const editBtn = document.getElementById('editBtn');
-    const copyBtn = document.getElementById('copyBtn');
-  
-    convertBtn.addEventListener('click', async () => {
-      const latex = latexInput.value;
-  
-      if (!latex) {
-        alert('Please enter a LaTeX expression to convert.');
-        return;
-      }
-  
-      try {
-        const mathml = await getMathML(latex);
-        mathmlOutput.value = mathml;
-      } catch (error) {
-        console.error('Error converting LaTeX to MathML:', error);
-        alert('An error occurred while converting LaTeX to MathML. Please check your input and try again.');
-      }
-    });
-  
-    editBtn.addEventListener('click', () => {
-      if (mathmlOutput.readOnly) {
-        mathmlOutput.readOnly = false;
-        editBtn.textContent = 'Disable Editing';
-      } else {
-        mathmlOutput.readOnly = true;
-        editBtn.textContent = 'Edit Output';
-      }
-    });
-  
-    copyBtn.addEventListener('click', () => {
-      mathmlOutput.select();
-      document.execCommand('copy');
-      alert('MathML code copied to clipboard.');
-    });
-  });
-  
+  const latexInput = document.getElementById('latexInput');
+  const convertBtn = document.getElementById('convertBtn');
+  const mathmlOutput = document.getElementById('mathmlOutput');
+  const editBtn = document.getElementById('editBtn');
+  const copyBtn = document.getElementById('copyBtn');
 
+
+  copyBtn.addEventListener('click', () => {
+    mathmlOutput.select();
+    document.execCommand('copy');
+    alert('MathML code copied to clipboard.');
+  });
+
+  convertBtn.addEventListener('click', async () => {
+    const latex = latexInput.value;
+
+    if (!latex) {
+      alert('Please enter a LaTeX expression to convert.');
+      return;
+    }
+
+    try {
+      const mathml = await getMathML(latex);
+      mathmlOutput.value = mathml;
+    } catch (error) {
+      console.error('Error converting LaTeX to MathML:', error);
+      alert('An error occurred while converting LaTeX to MathML. Please check your input and try again.');
+    }
+  });
   /*
   Funtion to sanitize user input to prevent XSS attacks.
 
@@ -68,11 +56,10 @@ document.addEventListener('DOMContentLoaded', () => {
   - Sanitizing input is an important security practice to prevent 
     potential security vulnerabilities such as XSS attacks.
   */
+
   function sanitizeInput(input) {
     return input.replace(/</g, '&lt;').replace(/>/g, '&gt;');
   }
-  
-  
   /*
   Function to convert the input string (LaTex) to MathML.
 
@@ -108,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   async function getMathML(latex) {
     const sanitizedInput = sanitizeInput(latex);
-  
+
     const mathJaxConfig = {
       display: false,
       em: 16,
@@ -118,10 +105,10 @@ document.addEventListener('DOMContentLoaded', () => {
       scale: 1,
       output: 'mathml',
     };
-  
+
     MathJax.texReset();
     const output = await MathJax.tex2mmlPromise(sanitizedInput, mathJaxConfig);
     const mathml = output.replace(/ xmlns="http:\/\/www.w3.org\/1998\/Math\/MathML"/g, '');
     return mathml;
   }
-  
+});
